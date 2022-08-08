37a3b684b4bba3addf3d0987f8ef8fa2127ce06a
Burak Özkırdeniz <burakozkirdeniz@gmail.com>
2022-08-08 19:44:25 +0300

    week2

---
+++ burak-ozkirdeniz/week2/hello-world/hardhat.config.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
+
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+module.exports = {
+   solidity: "0.8.9",
+   defaultNetwork: "goerli",
+   networks: {
+      hardhat: {},
+      goerli: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
